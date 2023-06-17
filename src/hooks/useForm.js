657e2dafa0993fd41bad542249845427fd83c6ce
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-06-17 21:40:53 +0530

    style+feat:login page

---
+++ src/hooks/useForm.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const useForm = (values) => {
+  const [form, setForm] = useState(values);
+
+  const handleChange = (e) => {
+    setForm((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  return {
+    formValues: form,
+    handleChange,
+  };
+};
+
+export default useForm;
