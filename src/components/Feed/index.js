ac314a377b8f4cc4994512b9ac6c0b9c8eefe03b
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-06-19 15:03:32 +0530

    style:Post page layout

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,26 +1,32 @@
 import React from "react";
-import { BsSearch } from "react-icons/bs";
 import Header from "../Header/Header";
-
+import Post from "../Post/Post";
 
 const Feed = () => {
-
   return (
     <div className="w-full h-full bg-[#FAFAFA]">
-      {/* <header className="w-full flex justify-around h-16 items-center shadow-lg ">
-        <div className="text-xl font-semibold tracking-wider">Instagram</div>
-        <div>
-        
-          <input
-          placeholder="search"
-            type="search"
-            name="search"
-            className="w-full px-2 py-1 transition rounded-lg bg-gray-100 border  outline-none hover:bg-transparent focus:bg-transparent placeholder:text-sm focus:border-gray-400"
-          />
+      <Header />
+
+      <div className="grid grid-cols-3 mt-20 w-full mx-auto max-w-screen-lg gap-6  ">
+        <div className="col-span-2  w-full  ">
+          <section className="flex items-center  overflow-x-scroll h-fit p-2 ">
+            {new Array(20).fill(0).map((_, index) => (
+              <div
+                key={index}
+                className=" w-14 h-14 border bg-black rounded-full ring-[2px]  ring-pink-500 ring-offset-2   mr-2  flex-none"
+              />
+            ))}
+          </section>
+          <section className=" flex flex-col gap-y-3 max-h-96">
+            {new Array(10).fill(0).map(() => {
+              return <Post />;
+            })}
+          </section>
         </div>
-        <div>all icons here </div>
-      </header> */}
-      <Header/>
+        <div className="col-span-1 bg-blue-400 h-fit  fixed  right-0 w-[8.3rem] md:w-[16rem] lg:w-[30rem] ">
+          tyhjvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvacxb nxxxxxxxxxs
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Post/Post.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { BsThreeDots } from "react-icons/bs";
+import { GrEmoji } from "react-icons/gr";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
+
+import { BsBookmark } from "react-icons/bs";
+import { IoShareOutline } from "react-icons/io5";
+
+const Post = () => {
+  return (
+    <div className="mainContainer w-full border border-gray-100  ">
+      <div className="1 upper flex bg-white p-2 justify-between">
+        <div className="flex items-center justify-center">
+          <div className="w-10 h-10 bg-gray-400 border border-pink-400 rounded-full " />
+          <div className="  ml-2">username</div>
+        </div>
+        <div className="items-center">
+          <BsThreeDots size={25} />
+        </div>
+      </div>
+      <div className="2 w-full h-96 bg-black aspect-square "></div>
+      <div className="3 flex justify-between">
+        <div className="flex  mt-2 space-x-3">
+          <div>
+            <AiOutlineHeart
+              size={25}
+              className="cursor-pointer hover:text-black/50"
+            />
+          </div>
+          <div>
+            <FaRegComment
+              size={25}
+              className="cursor-pointer hover:text-black/50"
+            />
+          </div>
+          <div>
+            <IoShareOutline
+              size={25}
+              className="cursor-pointer hover:text-black/50"
+            />
+          </div>
+        </div>
+        <div>
+          <BsBookmark
+            size={25}
+            className=" mt-2 cursor-pointer hover:text-black/50"
+          />
+        </div>
+      </div>
+      <div>100 likes</div>
+      <div>
+        {new Array(3).fill(1).map((_, i) => (
+          <div className="flex space-x-2">
+            <div className="font-semibold">username</div>
+            <div>comment{i + 1}</div>
+          </div>
+        ))}
+      </div>
+      <div className="">3 hours ago</div>
+      <div className="flex items-center w-full bg-white border border-l-gray-50">
+        <div className="m-2 bg-white">
+          <GrEmoji size={20} />
+        </div>
+        <form className="w-full">
+          <div className="flex border border-t-gray-100 border-l-white">
+            <input
+              type="text"
+              className="outline-none w-full h-10 bg-white  "
+              placeholder="Add a Comment..."
+            />
+            <button className="text-blue-600 font-bold mr-2">Post</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
